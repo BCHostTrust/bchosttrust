458e238588fbe49d4f9f692ca0f689f0b31afb4e
1F616EMO <root@1f616emo.xyz>
2023-12-16 19:13:22 +0800

    better error handling and related documentations

--- bchosttrust/consensus/pow.py
+++ bchosttrust/consensus/pow.py
@@ -17,7 +17,7 @@ def validate_block_hash(block: BCHTBlock):
     return validate_hash(block.hash)
 
 
-def attempt(version: int, prev_hash: bytes, creation_time: int, entries: tuple[BCHTEntry], maximum_tries: int = 4294967295, powf: typing.Callable = validate_block_hash) -> (typing.Union[BCHTBlock, None], int):
+def attempt(version: int, prev_hash: bytes, creation_time: int, entries: tuple[BCHTEntry], maximum_tries: int = BCHTBlock.MAX_NONCE, powf: typing.Callable = validate_block_hash) -> (typing.Union[BCHTBlock, None], int):
     """Attempt the proof-of-work by accuminating nonces
 
     Parameters
@@ -31,7 +31,7 @@ def attempt(version: int, prev_hash: bytes, creation_time: int, entries: tuple[B
     entries : tuple[BCHTEntry]
         A tuple of BCHTEntry objects.
     maximum_tries : int, optional
-        Maximum tries of the proof-of-work, by default and must not exceed 4294967295.
+        Maximum tries of the proof-of-work, by default and must not exceed BCHTBlock.MAX_NONCE.
     powf : function, optional
         Proof-of-work function accepting the BCHTBlock, by default validate_block_hash
 
@@ -41,8 +41,16 @@ def attempt(version: int, prev_hash: bytes, creation_time: int, entries: tuple[B
         The one satisfying the proof-of-work, or None if none found
     int
         Number of tries before finding one, or -1 if none found
+
+    Raises
+    ------
+    ValueError
+        If any BCHTBlock values are found to be invalid.
     """
 
+    if nonce > BCHTBlock.MAX_NONCE:
+        raise ValueError(
+            "nonce must not exceed {}".format(BCHTBlock.MAX_NONCE))
     for nonce in range(0, maximum_tries):
         block = BCHTBlock(version, prev_hash, creation_time, nonce, entries)
         if powf(block):
